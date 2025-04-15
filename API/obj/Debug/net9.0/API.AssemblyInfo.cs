fe3af0678d2c50d162b30acd8cdafc1c2a2be8bc
shenghaoisyummy <hsupisces@hotmail.com>
2025-04-15 21:34:30 +1000

    Initial React

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a74b2fbfbc61aa67765793f86ea23aa0034af23")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+592487b402c6f3f5cb106ca4403f88bb177f65a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
