6eaf412affe223b4bccc8205205d1001d4ceed3f
ShenghaoisYummy <hsupisces@Hotmail.com>
2025-04-20 21:17:28 +1000

    creat DbContext class

--- API/Program.cs
+++ API/Program.cs
@@ -1,14 +1,23 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddDbContext<StoreContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 builder.Services.AddCors();
 
 var app = builder.Build();
 
 
-app.UseCors(opt =>{
+app.UseCors(opt =>
+{
     opt.AllowAnyHeader()
        .AllowAnyMethod()
        .WithOrigins("http://localhost:3000");
--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4af1db7ae4a046d3608261b76e6861ccb2cfa577")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f80d4ed70352fd9a0376b516d6faa7d283eb6c13")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
