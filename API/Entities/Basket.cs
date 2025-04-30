1c9c25a82fabcf87cb38efdb1f370d743fb56997
ShenghaoisYummy <Hsupisces@hotmail.com>
2025-04-30 23:12:18 +1000

    add method t oadd and remove items

--- API/Entities/Basket.cs
+++ API/Entities/Basket.cs
@@ -7,4 +7,41 @@ public class Basket
     public int Id { get; set; }
     public required string BasketId { get; set; }
     public List<BasketItem> Items { get; set; } = [];
+
+    public void AddItem(Product product, int quantity)
+    {
+        if (product == null) ArgumentNullException.ThrowIfNull(product);
+        if (quantity <= 0) throw new ArgumentOutOfRangeException(nameof(quantity), "Quantity must be greater than zero.");
+
+        var existingItem = FindItem(product.Id);
+        if (existingItem == null)
+        {
+            Items.Add(new BasketItem
+            {
+                Quantity = quantity,
+                Product = product
+            });
+        }
+        else
+        {
+            existingItem.Quantity += quantity;
+        }
+    }
+
+    public void RemoveItem(int productId, int quantity)
+    {
+        if (quantity <= 0) throw new ArgumentException(nameof(quantity), "Quantity must be greater than zero.");
+        var item = FindItem(productId);
+
+        if (item == null) return;
+
+        if (item.Quantity <= 0) Items.Remove(item);
+        else item.Quantity -= quantity;
+    }
+
+
+    private BasketItem? FindItem(int productId)
+    {
+        return Items.FirstOrDefault(item => item.ProductId == productId);
+    }
 }
