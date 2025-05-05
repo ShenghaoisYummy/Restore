cce33f53f2c4aa2f0e230735d3975c555e588562
ShenghaoisYummy <hsupisces@Hotmail.com>
2025-05-05 23:53:29 +1000

    add the additem to the controller

--- API/Controllers/BasketController.cs
+++ API/Controllers/BasketController.cs
@@ -11,10 +11,7 @@ public class BasketController(StoreContext context) : BaseApiController
     [HttpGet]
     public async Task<ActionResult<Basket>> GetBseket()
     {
-        var basket = await context.Baskets
-            .Include(x => x.Items).
-            ThenInclude(x => x.Product).
-            FirstOrDefaultAsync(x => x.BasketId == Request.Cookies["basketId"]);
+        var basket = await RetrieveBasket();
 
         if (basket == null) return NoContent();
         return basket;
@@ -24,13 +21,50 @@ public class BasketController(StoreContext context) : BaseApiController
     [HttpPost]
     public async Task<ActionResult> AddItemToBasket(int productId, int quantity)
     {
-        return StatusCode(201);
+        var basket = await RetrieveBasket();
+
+        basket ??= CreateBasket();
+
+        var product = await context.Products.FindAsync(productId);
+
+        if (product == null) return BadRequest("Problem adding item to basket");
+
+        basket.AddItem(product, quantity);
+
+        var result = await context.SaveChangesAsync() > 0;
+
+        if (result) return CreatedAtAction(nameof(GetBseket), basket);
+
+        return BadRequest("Problem adding item to basket");
+    }
+
+    private async Task<Basket?> RetrieveBasket()
+    {
+        return await context.Baskets
+            .Include(x => x.Items)
+            .ThenInclude(x => x.Product)
+            .FirstOrDefaultAsync(x => x.BasketId == Request.Cookies["basketId"]);
     }
 
     [HttpDelete]
     public async Task<ActionResult> RemoveBasketItem(int productId, int quantity)
-    
+
     {
         return StatusCode(201);
+    }
+
+    private Basket CreateBasket()
+    {
+        var basketId = Guid.NewGuid().ToString();
+        var cookieOptions = new CookieOptions
+        {
+            IsEssential = true,
+            Expires = DateTimeOffset.UtcNow.AddDays(30),
+        };
+        Response.Cookies.Append("basketId", basketId, cookieOptions);
+        var basket = new Basket { BasketId = basketId };
+        context.Baskets.Add(basket);
+        return basket;
+
     }
 }
--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+444c28db2181aa6e8499ff7223123c7b8272b930")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf5c741b74131f66523b50dc61f5139ae4c43e17")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
