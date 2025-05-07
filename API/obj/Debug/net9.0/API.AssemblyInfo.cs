874993b45769f1841d6d4baafa41cbf67d5ccd16
ShenghaoisYummy <hsupisces@Hotmail.com>
2025-05-07 16:55:05 +1000

    Init .net core debugger json

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf5c741b74131f66523b50dc61f5139ae4c43e17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cce33f53f2c4aa2f0e230735d3975c555e588562")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
