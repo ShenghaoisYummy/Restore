c890c355531eda2e9683d474a50c32feb7856a6d
ShenghaoisYummy <hsupisces@Hotmail.com>
2025-07-05 14:41:22 +1000

    add an endpoint to get the filters from API

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f430138d28eea8f8ea601013a130afc59380d7cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03cd60c5a9d01401b3834406fc035f83fdd1c3c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
