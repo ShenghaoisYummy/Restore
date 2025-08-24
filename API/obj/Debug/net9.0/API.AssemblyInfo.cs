307f0b52e71cb7e8bc5a142c5aa010b48b372166
ShenghaoisYummy <hsupisces@Hotmail.com>
2025-08-24 15:05:09 +1000

    Add a service to deal with image upload

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dc4ac1bbcfe63e7425d51665e8b5d21fe1438c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9dc34b24de8fa1d1e4bdebafa343d8fe9fee806")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
